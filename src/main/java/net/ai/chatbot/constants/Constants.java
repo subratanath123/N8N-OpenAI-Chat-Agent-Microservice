52c5a7a1fe077443f62bb5a46a0afd6d733eb825
subratanath123 <shuvra.dev9@gmail.com>
2026-01-16 20:35:55 +0600

    Fixed redi stream name

--- src/main/java/net/ai/chatbot/constants/Constants.java
+++ src/main/java/net/ai/chatbot/constants/Constants.java
@@ -2,7 +2,7 @@ package net.ai.chatbot.constants;
 
 public class Constants {
 
-    public static final String CHAT_BOT_CREATE_EVENT_STREAM = "chatbot-create-event";
+    public static final String CHAT_BOT_CREATE_EVENT_STREAM = "chatbot-create-eventstream";
     //Have to make this dybamic so that multiple application get a unique stream consumer group
     public static final String REDIS_STREAM_SERVER_GROUP = "redis-stream-server-group-1";
 
